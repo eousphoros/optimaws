a5dad116c44a1b1816f4ee85803bb4bfeb3556ba
Patrick Buckley <ptrck@blck.io>
2014-03-03 09:11:54 +0000

    Readability tinkering

--- routes/fail.rb
+++ routes/fail.rb
@@ -3,7 +3,7 @@ class Optimaws < Sinatra::Application
 	get '/fail' do
           pass if request.path_info == '/playground/favicon.ico'
           @title = 'OptimAWS'
-          @output   = "Oops! We are not sure what to do with that."
+          @output   = "Oops! We are not sure what to do with that request."
           @loc      = 'Error'
           haml :fail
 	end
